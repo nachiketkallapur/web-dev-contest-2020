f354001a382811d5faf4e684fe08919e97cbebc3
nachiketkallapur <nachiketkallapur@gmail.com>
2020-03-20 14:53:00 +0530

    Updates

---
+++ precache-manifest.633da8dce0e3eddc61522c609dc904f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0752f6cbcbee394097fdf9502e392a",
+    "url": "/web-dev-contest-2020/index.html"
+  },
+  {
+    "revision": "c9653abf73404fec645a",
+    "url": "/web-dev-contest-2020/static/css/main.e9649d0d.chunk.css"
+  },
+  {
+    "revision": "1594e3702fcdc19b063f",
+    "url": "/web-dev-contest-2020/static/js/2.ff1886ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/web-dev-contest-2020/static/js/2.ff1886ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9653abf73404fec645a",
+    "url": "/web-dev-contest-2020/static/js/main.ea709c37.chunk.js"
+  },
+  {
+    "revision": "8ea0d7200843176edd1c",
+    "url": "/web-dev-contest-2020/static/js/runtime-main.6eb087f7.js"
+  }
+]);
